3fae4710ccff21a0598d7afe9bc6e4bb6f7d04b3
anhhoang <anhhoang@ufl.edu>
2019-03-07 12:47:05 -0500

    Add onsuccsess that can update and user gmail data from google login and logged into the homepage

--- client/js/login.js
+++ client/js/login.js
@@ -5,87 +5,48 @@
 
 // auth.onAuthStateChanged(firebaseUser => {});
 
-  // Initialize Firebase
-  var config = {
-      apiKey: "AIzaSyCvsB1qBMOpUOG0XT9TM9ShTouhQjf_hZs",
-      authDomain: "mentormatch-1551397019664.firebaseapp.com",
-      databaseURL: "https://mentormatch-1551397019664.firebaseio.com",
-      projectId: "mentormatch-1551397019664",
-      storageBucket: "mentormatch-1551397019664.appspot.com",
-      messagingSenderId: "94097431082"
-  };
-  firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCvsB1qBMOpUOG0XT9TM9ShTouhQjf_hZs",
+        authDomain: "mentormatch-1551397019664.firebaseapp.com",
+        databaseURL: "https://mentormatch-1551397019664.firebaseio.com",
+        projectId: "mentormatch-1551397019664",
+        storageBucket: "mentormatch-1551397019664.appspot.com",
+        messagingSenderId: "94097431082"
+    };
+    firebase.initializeApp(config);
 
-  const textEditEmail = document.getElementById('textEditEmail');
-  const textEditPassword = document.getElementById('textEditPassword');
-  const buttonLogin = document.getElementById('buttonLogin');
-  const buttonSignup = document.getElementById('buttonSignup');
-
-// var provider = new firebase.auth.GoogleAuthProvider();
-
-// var signinWithGoogle = firebase.auth().signInWithPopup(provider).then(function(result)
-// {
-//     // This gives you a Google Access Token. You can use it to access the Google API.
-//     var token = result.credential.accessToken;
-//     // The signed-in user info.
-//     var user = result.user;
-//     // ...
-//     console.log("Success log in with google");
-// }).catch(function(error)
-// {
-//     // Handle Errors here.
-//     var errorCode = error.code;
-//     var errorMessage = error.message;
-//     // The email of the user's account used.
-//     var email = error.email;
-//     // The firebase.auth.AuthCredential type that was used.
-//     var credential = error.credential;
-//     // ...
-//     console.log("Not Successfull log in with google!")
-// });
-
-firebase.auth().getRedirectResult().then(function(result)
-{
-    if (result.credential)
+    const textEditEmail = document.getElementById('textEditEmail');
+    const textEditPassword = document.getElementById('textEditPassword');
+    const buttonLogin = document.getElementById('buttonLogin');
+    const buttonSignup = document.getElementById('buttonSignup');
+    const buttonGoogle = document.getElementById('googleButton');
+    var clickedGoogleButton = false;
+    function onSignin(googleUser)
     {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // ...
+        // Useful data for your client-side scripts:
+        if (clickedGoogleButton)
+        {
+            var profile = googleUser.getBasicProfile();
+            console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+            console.log('Full Name: ' + profile.getName());
+            console.log('Given Name: ' + profile.getGivenName());
+            console.log('Family Name: ' + profile.getFamilyName());
+            console.log("Image URL: " + profile.getImageUrl());
+            console.log("Email: " + profile.getEmail());
+
+            // The ID token you need to pass to your backend:
+            // var id_token = googleUser.getAuthResponse().id_token;
+            // console.log("ID Token: " + id_token);
+            window.location.href = "index.html";
+        }
     }
-    // The signed-in user info.
-    var user = result.user;
-}).catch(function(error)
-{
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
+    buttonGoogle.addEventListener('click', e=>
+    {
+        clickedGoogleButton = true;
+    })
 
-    console.log("Not Successful redirect!");
-    // ...
-});
-
-
-function onSignin(googleUser)
-{
-    // Useful data for your client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-}
-
-  buttonLogin.addEventListener('click', e =>
+    buttonLogin.addEventListener('click', e =>
     {
         const email = textEditEmail.value;
         const password = textEditPassword.value;
@@ -95,8 +56,7 @@ function onSignin(googleUser)
             .catch(e => console.log(e.message));
     })
 
-
-  buttonSignup.addEventListener('click', e =>
+    buttonSignup.addEventListener('click', e =>
     {
         const email = textEditEmail.value;
         const password = textEditPassword.value;
@@ -106,18 +66,17 @@ function onSignin(googleUser)
             .catch(e => console.log(e.message));
     })
 
-  firebase.auth().onAuthStateChanged(firebaseUser =>
+    firebase.auth().onAuthStateChanged(firebaseUser =>
     {
         if (firebaseUser)
         {
             console.log("Sign up successful");
             // window.location.href = "index.html";
         }
-        dcdfgcvghjelse
-            console.log("Not successful");
+        console.log("Not successful");
     });
 
-  buttonLogin.addEventListener('click', e =>
+    buttonLogin.addEventListener('click', e =>
     {
         const email = textEditEmail.value;
         const password = textEditPassword.value;
@@ -128,3 +87,4 @@ function onSignin(googleUser)
         answer
             .catch(e => console.log(e.message));
     })
+

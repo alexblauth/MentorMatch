811f2384b8923dc5a5022a5a4c0b1a915f9714c5
anhhoang <anhhoang@ufl.edu>
2019-03-07 12:01:08 -0500

    linked when successfully logged in to index.html

--- client/js/login.js
+++ client/js/login.js
@@ -20,30 +20,29 @@
   const textEditPassword = document.getElementById('textEditPassword');
   const buttonLogin = document.getElementById('buttonLogin');
   const buttonSignup = document.getElementById('buttonSignup');
-  const buttonLoginGoogle = document.getElementById('buttonLoginGoogle');
 
-var provider = new firebase.auth.GoogleAuthProvider();
+// var provider = new firebase.auth.GoogleAuthProvider();
 
-var signinWithGoogle = firebase.auth().signInWithPopup(provider).then(function(result)
-{
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-    console.log("Success log in with google");
-}).catch(function(error)
-{
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-    console.log("Not Successfull log in with google!")
-});
+// var signinWithGoogle = firebase.auth().signInWithPopup(provider).then(function(result)
+// {
+//     // This gives you a Google Access Token. You can use it to access the Google API.
+//     var token = result.credential.accessToken;
+//     // The signed-in user info.
+//     var user = result.user;
+//     // ...
+//     console.log("Success log in with google");
+// }).catch(function(error)
+// {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     // The email of the user's account used.
+//     var email = error.email;
+//     // The firebase.auth.AuthCredential type that was used.
+//     var credential = error.credential;
+//     // ...
+//     console.log("Not Successfull log in with google!")
+// });
 
 firebase.auth().getRedirectResult().then(function(result)
 {
@@ -88,7 +87,12 @@ function onSignin(googleUser)
 
   buttonLogin.addEventListener('click', e =>
     {
-        signinWithGoogle;
+        const email = textEditEmail.value;
+        const password = textEditPassword.value;
+        const auth = firebase.auth();
+        const answer = auth.signInWithEmailAndPassword(email, password);
+        answer
+            .catch(e => console.log(e.message));
     })
 
 
@@ -105,8 +109,11 @@ function onSignin(googleUser)
   firebase.auth().onAuthStateChanged(firebaseUser =>
     {
         if (firebaseUser)
+        {
             console.log("Sign up successful");
-        else
+            // window.location.href = "index.html";
+        }
+        dcdfgcvghjelse
             console.log("Not successful");
     });
 
@@ -117,13 +124,7 @@ function onSignin(googleUser)
         const auth = firebase.auth();
         const answer = auth.signInWithEmailAndPassword(email, password);
         if (answer)
-            window.location.href = "successLogin.html";
+            window.location.href = "index.html";
         answer
             .catch(e => console.log(e.message));
     })
-
-function onSignIn(googleUser)
-{
-    var profile = googleUser.getBasicProfile();
-    console.log(profile.getEmail());
-}

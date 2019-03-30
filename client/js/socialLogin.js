888e59adb942fd60660477c28c4948b61c70f746
anhhoang <anhhoang@ufl.edu>
2019-03-30 14:11:50 -0400

    add all files

---
+++ client/js/socialLogin.js
@@ -0,0 +1,25 @@
+
+var googleUser = {};
+var startApp = function() {
+    gapi.load('auth2', function(){
+        // Retrieve the singleton for the GoogleAuth library and set up the client.
+        auth2 = gapi.auth2.init({
+            client_id: '94097431082-u080o437bpmes48td7lh5ojcungl8hsn.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin',
+            // Request scopes in addition to 'profile' and 'email'
+            //scope: 'additional_scope'
+        });
+        attachSignin(document.getElementById('customBtn'));
+    });
+};
+
+function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+            document.getElementById('name').innerText = "Signed in: " +
+                googleUser.getBasicProfile().getName();
+        }, function(error) {
+            alert(JSON.stringify(error, undefined, 2));
+        });
+}
